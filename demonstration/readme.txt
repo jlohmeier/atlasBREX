487c66bff7e74cb1200e0bc1a142e9f927811bf2
Johannes Lohmeier <mail@jlohmeier.de>
2017-05-30 17:17:06 +0200

    Demonstrational dataset

---
+++ demonstration/readme.txt
@@ -0,0 +1,18 @@
+#Demonstration using averaged template 
+#Dataset: Developmental rhesus macaque dataset (T2W) at age 20-24 months (UNC-Wisconsin Neurodevelopment Rhesus Database*)
+#Averaged template construction (n=13) using ABSORB: Atlas Building by Self-Organized Registration and Bundling
+
+Usage: 
+bash siterate.sh
+
+Note:
+-this example requires the installation of ANTs (and FSL)
+-output/results are provided in the results folder
+- *_brain_lin.nii.gz show results from linear registration, while *_brain.nii.gz demonstrate results from non-linear registration
+
+/*****************************/
+
+*Dataset:
+Young JT et al. The UNC-Wisconsin Rhesus Macaque Neurodevelopment Database: A Structural MRI and DTI Database of Early Postnatal Development. Frontiers in Neuroscience. 2017 Feb 2;11:292–11.
+https://www.nitrc.org/projects/uncuw_macdevmri/
+https://data.kitware.com/#collection/54b582c38d777f4362aa9cb3/folder/553e6db18d777f082b5918eb
---
+++ demonstration/siterate.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+#fractional intensity should be determined prior running atlasBREX
+#nohup siterate.sh &
+
+echo "PID: $BASHPID"
+
+for file in *'sj_'*'.nii.gz'*
+do
+
+btemplate=b_avg_2024mo_brain_blur4.nii.gz
+nbtemplate=nb_avg_2024mo_blur4.nii.gz
+
+sh atlasBREX.sh -b $btemplate -nb $nbtemplate -h $file -f 0.5 -reg 1 -w 5,5,5 -msk a,0,0
+wait
+
+name=$(echo $file | cut -d "_" -f2- )
+
+#remove prefix and rename
+mv -f ${file%%.*}_brain.nii.gz FNIRT_${name%%.*}_brain.nii.gz 
+mv -f ${file%%.*}_brain_lin.nii.gz FNIRT_${name%%.*}_brain_lin.nii.gz 
+
+#remove warp files and matrix
+rm -f *std2high*
+
+wait 
+
+sh atlasBREX.sh -b $btemplate -nb $nbtemplate -h $file -f 0.5 -reg 2 -w 1 -msk a,0,0
+wait
+
+#remove prefix and rename
+mv -f ${file%%.*}_brain.nii.gz ANTS_${name%%.*}_brain.nii.gz 
+mv -f ${file%%.*}_brain_lin.nii.gz ANTS_${name%%.*}_brain_lin.nii.gz 
+
+#remove warp files and matrix
+rm -f *std2high*
+
+wait 
+
+done
+
